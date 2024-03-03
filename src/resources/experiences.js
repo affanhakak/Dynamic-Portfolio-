ca4dbc3606439cd37c9853a35dabcd189566d443
Affan Hakak <affanshowkat@gmail.com>
2024-03-03 13:46:27 +0530

    added data for experiences

---
+++ src/resources/experiences.js
@@ -0,0 +1,16 @@
+export const experiences = [
+  {
+    _id: "630120134042e2d798b59dc1",
+    company: "Planetcast Media Services Limited",
+    title: "Software Engineer",
+    period: "Jul-22 to Present",
+    techUsed: "Angular, React, Node, Express, MongoDB, Redux, RxJS",
+  },
+  {
+    _id: "630120134042e2d798b59dc0",
+    company: "Collabera Technologies",
+    title: "Software Developer",
+    period: "Mar-22 to Jul-22",
+    techUsed: "Angular, RxJS, React, Redux",
+  },
+];
